2f9ba6da48e38e52891c786a8adc1fb7a2f77fd6
venn <wxchunjhyy@163.com>
2019-12-10 15:21:28 +0800

    add AfterMatchStrategy Demo for test After match strategy 2019-12-10 15:21:28

---
+++ src/main/test/com/venn/kafka/KafkaOffsetRevertTest.scala
@@ -0,0 +1,47 @@
+package com.venn.kafka
+
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+
+import com.venn.common.Common
+import com.venn.util.MathUtil
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import scala.util.parsing.json.JSONObject
+
+/**
+  * test data maker
+  */
+
+object CurrentDayMaker {
+
+
+  /**
+    * kafka offset revert test
+    * kafka offset 回退测试
+    *
+    * @return
+    */
+  val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+
+  def main(args: Array[String]): Unit = {
+    val producer = new KafkaProducer[String, String](Common.getProp)
+    var i = 0;
+    while (true) {
+
+      //      val map = Map("id"-> i, "createTime"-> sdf.format(System.currentTimeMillis()))
+      val map = Map("id" -> i, "createTime" -> sdf.format(System.currentTimeMillis()), "amt" -> (MathUtil.random.nextInt(10) + "." + MathUtil.random.nextInt(10)))
+      val jsonObject: JSONObject = new JSONObject(map)
+      println(jsonObject.toString())
+      // topic current_day
+      val msg = new ProducerRecord[String, String]("kafka_offset", jsonObject.toString())
+      producer.send(msg)
+      producer.flush()
+      Thread.sleep(1000)
+      i = i + 1
+      //      System.exit(-1)
+    }
+  }
+
+}
+
