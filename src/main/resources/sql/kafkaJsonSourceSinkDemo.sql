93ea38ff6be365a66cbfebca5520b5f24058bd72
venn <wxchunjhyy@163.com>
2020-03-15 18:01:01 +0800

    update today 2020-03-15 18:01:01

---
+++ src/main/resources/sql/kafkaJsonSourceSinkDemo.sql
@@ -0,0 +1,40 @@
+--sourceTable
+CREATE TABLE user_log(
+    user_id VARCHAR,
+    item_id VARCHAR,
+    category_id VARCHAR,
+    behavior VARCHAR,
+    ts TIMESTAMP(3)
+) WITH (
+    'connector.type' = 'kafka',
+    'connector.version' = 'universal',
+    'connector.topic' = 'user_behavior',
+    'connector.properties.zookeeper.connect' = 'venn:2181',
+    'connector.properties.bootstrap.servers' = 'venn:9092',
+    'connector.startup-mode' = 'earliest-offset',
+    'format.type' = 'json'
+#    'format.type' = 'csv'
+);
+
+--sinkTable
+CREATE TABLE user_log_sink (
+    user_id VARCHAR,
+    item_id VARCHAR,
+    category_id VARCHAR,
+    behavior VARCHAR,
+    ts TIMESTAMP(3)
+) WITH (
+    'connector.type' = 'kafka',
+    'connector.version' = 'universal',
+    'connector.topic' = 'user_behavior_sink',
+    'connector.properties.zookeeper.connect' = 'venn:2181',
+    'connector.properties.bootstrap.servers' = 'venn:9092',
+    'update-mode' = 'append',
+#    'format.type' = 'json'
+     'format.type' = 'csv'
+);
+
+--insert
+INSERT INTO user_log_sink(dt, pv, uv)
+SELECT user_id, item_id, category_id, behavior, ts
+FROM user_log;
