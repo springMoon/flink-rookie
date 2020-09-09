5324b1d6cabdab7d1de586118c8aa2f51cd92c4b
venn <wxchunjhyy@163.com>
2020-09-09 19:45:58 +0800

    format it 2020-09-09 19:45:58

--- src/main/scala/com/venn/questing/dynamicWindow/DyWindowDemo.scala
+++ src/main/scala/com/venn/questing/dynamicWindow/DyWindowDemo.scala
@@ -1,11 +1,9 @@
 package com.venn.index.question.dynamicWindow
 
-import java.text.SimpleDateFormat
 import java.util
 
 import com.google.gson.Gson
 import com.venn.common.Common
-import org.apache.flink.api.common.eventtime.{TimestampAssignerSupplier, WatermarkGenerator, WatermarkGeneratorSupplier, WatermarkStrategy}
 import org.apache.flink.api.common.functions.RichFlatMapFunction
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.common.state.MapStateDescriptor
@@ -15,11 +13,8 @@ import org.apache.flink.configuration.Configuration
 import org.apache.flink.runtime.state.StateBackend
 import org.apache.flink.runtime.state.filesystem.FsStateBackend
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction
-import org.apache.flink.streaming.api.scala.{DataStream, OutputTag, StreamExecutionEnvironment}
-import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.flink.util.Collector
 import org.slf4j.LoggerFactory
@@ -97,17 +92,11 @@ object DyWindowDemo {
         var currentCommand: util.HashMap[String, Command] = _
         // 存放新命令的 map
         var commandState: MapStateDescriptor[String, Command] = _
-        //        var sumTag: OutputTag[(DataEntity, Command)] = _
-        //        var minTag: OutputTag[(DataEntity, Command)] = _
-        //        var maxTag: OutputTag[(DataEntity, Command)] = _
 
         override def open(parameters: Configuration): Unit = {
 
           currentCommand = new util.HashMap[String, Command]()
           commandState = new MapStateDescriptor[String, Command]("commandState", BasicTypeInfo.STRING_TYPE_INFO, TypeInformation.of(new TypeHint[Command]() {}))
-          //          sumTag = new OutputTag[(DataEntity, Command)]("sum")
-//          minTag = new OutputTag[(DataEntity, Command)]("min")
-//          maxTag = new OutputTag[(DataEntity, Command)]("max")
         }
 
         override def processElement(element: DataEntity, ctx: BroadcastProcessFunction[DataEntity, Command, (DataEntity, Command)]#ReadOnlyContext, out: Collector[(DataEntity, Command)]): Unit = {
@@ -122,15 +111,6 @@ object DyWindowDemo {
           }
           // 如果当前命令为空，数据就不往下发送了
           if (current != null) {
-            //            current.method match {
-            //              case "sum" =>
-            //                ctx.output(sumTag, (element, current))
-            //              case "min" =>
-            //                ctx.output(minTag, (element, current))
-            //              case "max" =>
-            //                ctx.output(maxTag, (element, current))
-            //              case _ =>
-            //            }
             out.collect((element, current))
           }
           // command not exists, ignore it
@@ -144,40 +124,11 @@ object DyWindowDemo {
       })
       .assignAscendingTimestamps(_._1.time)
 
-    //    connectStream.print()
-
     // todo process sum
     val sumStream = connectStream
       .keyBy(_._1.attr)
       .window(DynamicTumblingEventTimeWindows.of())
       .process(new DyProcessWindowFunction("sum"))
-      /*.process(new ProcessWindowFunction[(DataEntity, Command), String, String, TimeWindow] {
-        override def process(key: String, context: Context, elements: Iterable[(DataEntity, Command)], out: Collector[String]): Unit = {
-          // start-end
-          val taskId = elements.head._2.taskId
-          val method = elements.head._2.method
-          val targetAttr = elements.head._2.targetAttr
-          val periodStartTime = context.window.getStart
-          val periodEndTime = context.window.getEnd
-
-          var value: Double = 0d
-
-          val it = elements.toIterator
-          while (it.hasNext) {
-            value += it.next()._1.value
-          }
-          val buffer = new StringBuilder
-          buffer.append(taskId).append(Common.split)
-          buffer.append(method).append(Common.split)
-          buffer.append(targetAttr).append(Common.split)
-          val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-          buffer.append(sdf.format(periodStartTime)).append(Common.split)
-          buffer.append(sdf.format(periodEndTime)).append(Common.split)
-          buffer.append(value)
-
-          out.collect(buffer.toString())
-        }
-      })*/
       .print("sum")
 
     env.execute("DyWindowDemo")
