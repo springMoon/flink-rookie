ccb4a3760aeb77a2ea4ab4f4503b49a7526ea70a
venn <wxchunjhyy@163.com>
2020-09-11 11:33:40 +0800

    update BroadcastProcessFunction to KeyedBroadcastProcessFunction, story currentCommandState to statebackend

--- src/main/scala/com/venn/questing/dynamicWindow/DyWindowDemo.scala
+++ src/main/scala/com/venn/questing/dynamicWindow/DyWindowDemo.scala
@@ -6,14 +6,14 @@ import com.google.gson.Gson
 import com.venn.common.Common
 import org.apache.flink.api.common.functions.RichFlatMapFunction
 import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.api.common.state.MapStateDescriptor
+import org.apache.flink.api.common.state.{MapState, MapStateDescriptor}
 import org.apache.flink.api.common.typeinfo.{BasicTypeInfo, TypeHint, TypeInformation}
 import org.apache.flink.api.scala._
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.runtime.state.StateBackend
 import org.apache.flink.runtime.state.filesystem.FsStateBackend
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction
+import org.apache.flink.streaming.api.functions.co.{BroadcastProcessFunction, KeyedBroadcastProcessFunction}
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.flink.util.Collector
@@ -41,6 +41,7 @@ object DyWindowDemo {
     val commandStream = env.addSource(commandSource)
       .flatMap(new RichFlatMapFunction[String, Command] {
         var gson: Gson = _
+
         override def open(parameters: Configuration): Unit = {
           gson = new Gson()
         }
@@ -84,21 +85,23 @@ object DyWindowDemo {
 
     // connect stream
     val connectStream = dataStream
+      .keyBy(_.attr)
       .connect(commandStream)
-      .process(new BroadcastProcessFunction[DataEntity, Command, (DataEntity, Command)]() {
+      .process(new KeyedBroadcastProcessFunction[String, DataEntity, Command, (DataEntity, Command)]() {
 
         // 存放当前命令的 map
-        var currentCommand: util.HashMap[String, Command] = _
+        var currentCommand: MapState[String, Command] = _
         // 存放新命令的 map
         var commandState: MapStateDescriptor[String, Command] = _
 
         override def open(parameters: Configuration): Unit = {
 
-          currentCommand = new util.HashMap[String, Command]()
+          currentCommand = getRuntimeContext.getMapState(new MapStateDescriptor[String, Command]("currentCommand", BasicTypeInfo.STRING_TYPE_INFO, TypeInformation.of(new TypeHint[Command]() {})))
           commandState = new MapStateDescriptor[String, Command]("commandState", BasicTypeInfo.STRING_TYPE_INFO, TypeInformation.of(new TypeHint[Command]() {}))
         }
 
-        override def processElement(element: DataEntity, ctx: BroadcastProcessFunction[DataEntity, Command, (DataEntity, Command)]#ReadOnlyContext, out: Collector[(DataEntity, Command)]): Unit = {
+        override def processElement(element: DataEntity, ctx: KeyedBroadcastProcessFunction[String, DataEntity, Command, (DataEntity, Command)]#ReadOnlyContext, out: Collector[(DataEntity, Command)]): Unit = {
+
           // 命令可以是大于/小于当前时间
           // 小于当前时间的，直接添加即可,之前命令的窗口不会收到新数据，新数据直接进新命令的窗口
           // 大于当前时间的命令，不能直接与流一起往下游输出，等时间小于当前的 processTime 时间后，才会开始新窗口
@@ -115,11 +118,12 @@ object DyWindowDemo {
           // command not exists, ignore it
         }
 
-        override def processBroadcastElement(element: Command, ctx: BroadcastProcessFunction[DataEntity, Command, (DataEntity, Command)]#Context, out: Collector[(DataEntity, Command)]): Unit = {
+        override def processBroadcastElement(element: Command, ctx: KeyedBroadcastProcessFunction[String, DataEntity, Command, (DataEntity, Command)]#Context, out: Collector[(DataEntity, Command)]): Unit = {
           // only one command are new accepted, cover old command
           logger.info("receive command : " + element)
           ctx.getBroadcastState(commandState).put(element.targetAttr, element)
         }
+
       })
       .assignAscendingTimestamps(_._1.time)
 
@@ -132,7 +136,6 @@ object DyWindowDemo {
 
     env.execute("DyWindowDemo")
   }
-
 }
 
 case class Command(taskId: String, targetAttr: String, method: String, periodUnit: String, periodLength: Long, startTime: Long)
