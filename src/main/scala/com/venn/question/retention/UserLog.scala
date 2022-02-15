cb2936ced1153e6f7bf8b2ecf5ef03a50ba5ada6
wuxu <wxchunjhyy@163.com>
2022-02-15 15:51:53 +0800

    finish retention

--- src/main/scala/com/venn/question/retention/UserLog.scala
+++ src/main/scala/com/venn/question/retention/UserLog.scala
@@ -1,3 +1,3 @@
 package com.venn.question.retention
 
-case class UserLog(userId: String, categoryId: Int, itemId: Int, behavior: String, ts: String)
+case class UserLog(userId: String, categoryId: Int, itemId: Int, behavior: String, ts: String, tsLong: Long)
