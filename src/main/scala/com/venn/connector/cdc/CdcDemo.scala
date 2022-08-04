b1bc04eb85567021829aa86af4b9425e1431e22b
venn <wxchunjhyy@163.com>
2022-08-04 16:52:06 +0800

    udpate

---
+++ src/main/scala/com/venn/connector/cdc/CdcDemo.scala
@@ -0,0 +1,17 @@
+package com.venn.connector.cdc
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+class CdcDemo {
+
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // source
+    val cdcSource =
+
+
+  }
+
+}
