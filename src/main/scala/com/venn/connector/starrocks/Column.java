8f795b3178307d4c566639fdf668a52e5c2d81a1
venn <wxchunjhyy@163.com>
2024-03-08 15:16:38 +0800

    finish auto create starrocks table

---
+++ src/main/scala/com/venn/connector/starrocks/Column.java
@@ -0,0 +1,7 @@
+package com.venn.connector.starrocks;/**
+ * @Classname Column
+ * @Description TODO
+ * @Date 2024/3/8
+ * @Created by venn
+ */public class Column {
+}
---
+++ src/main/scala/com/venn/connector/starrocks/TableSchema.java
@@ -0,0 +1,7 @@
+package com.venn.connector.starrocks;/**
+ * @Classname TableSchema
+ * @Description TODO
+ * @Date 2024/3/8
+ * @Created by venn
+ */public class TableSchema {
+}
