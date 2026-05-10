4121989a2a47c463d9d15bd50356861478dc3b5d
venn <wxchunjhyy@163.com>
2026-05-10 11:02:39 +0800

    update

---
+++ src/main/java/com/venn/demo/KafkaDemo.java
@@ -0,0 +1,48 @@
+package com.venn.demo;
+
+import com.venn.entity.KafkaSimpleStringRecord;
+import com.venn.util.SimpleKafkaRecordDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.concurrent.TimeUnit;
+
+public class KafkaDemo {
+
+    private static final String uri = "redis://localhost";
+    private static final String bootstrapServer = "10.201.0.191:9092";
+    private static final String topic = "employee";
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // kafka source
+        KafkaSource<KafkaSimpleStringRecord> kafkaSource = KafkaSource
+                .<KafkaSimpleStringRecord>builder()
+                .setBootstrapServers(bootstrapServer)
+                .setDeserializer(new SimpleKafkaRecordDeserializationSchema())
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setTopics(topic)
+                .build();
+
+        // get value
+        SingleOutputStreamOperator<String> source = env
+                .fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "kafkaSource")
+                .map((MapFunction<KafkaSimpleStringRecord, String>) value -> value.getValue())
+                .disableChaining();
+
+
+        // print result
+        source
+                .print("data:");
+
+        env.execute("KafkaDemo");
+    }
+}
